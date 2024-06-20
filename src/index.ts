735deb24134921839fb409027d7407fd94552b96
DeOliveiraDeveloper <monteirocaio4@gmail.com>
2024-06-19 23:42:28 -0300

    Rotas

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,49 @@ import express from 'express'
 
 const servidor = express()
 
-servidor.get('/', (req, res) => {
-    return res.send("Server ON")
+servidor.get('/produtos/:item', (req, res) => {
+    console.log(req.params);
+    return res.send("Server ON, Pai")
+})
+
+const pessoa = [
+    {
+        nome: "Eu",
+        sobrenome: "Tu",
+        apelido: "Nós",
+        vulgo: "Magrelin"
+    },
+    {
+        nome: "1",
+        sobrenome: "2",
+        apelido: "3",
+        vulgo: "4"
+    },
+    {
+        nome: "5",
+        sobrenome: "6",
+        apelido: "7",
+        vulgo: "8"
+    },
+    {
+        nome: "9",
+        sobrenome: "10",
+        apelido: "9",
+        vulgo: "8 kkkk"
+    },
+]
+
+servidor.get('/usuarios/:vulgo', (req, res) => {
+    const {vulgo} = req.params
+
+    const pessoas = pessoa.find((item) => {
+        return item.vulgo === vulgo
+    })
+
+    if (!pessoas) {
+        return res.send("Barro negão.")
+    }
+    return res.send(pessoas)
 })
 
 servidor.listen(process.env.PORT)
